b6e49f95b15c10ccfd77921172d6c6e94196824a
Yudai TANAKA <63273927+yudaitanaka1026@users.noreply.github.com>
2023-10-20 09:36:32 +0900

    Update 1-2_Japanese_Comprehension.py

--- 1-2_Japanese_Comprehension.py
+++ 1-2_Japanese_Comprehension.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: tanakayudai
+@author: Yudai Tanaka
 
 Solve the original Japanese text comprehension questions of the NMLE in Japan by ChatGPT API
 
-"OPENAI_API_KEY", "CSV_FILE_NAME", "QUESTION_FOLDER_PATH" and "RESULT_FOLDER_PATH" should be set according to your environment following the comments below.
-
-- `OPENAI_API_KEY`: Your OpenAI API key
-- `CSV_FILE_NAME`: Name of your question CSV file
-- `QUESTION_FOLDER_PATH`: Path to your question CSV file's folder
-- `RESULT_FOLDER_PATH`: Path to save the result
+Setup requires:
+- `YOUR_OPENAI_API_KEY`: Your OpenAI API key
+- `YOUR_QUESTION_CSV_FILE_NAME`: Name of your question CSV file
+- `PATH_TO_QUESTION_FOLDER`: Path to your question CSV file's folder
+- `PATH_TO_RESULT_FOLDER`: Path to save the result
 """
 
 # Install the packages
 !pip install openai
 import openai
 import pandas as pd
-openai.api_key = "OPENAI_API_KEY"
+openai.api_key = "YOUR_OPENAI_API_KEY"
 
 # Answer the Original Japanese questions
 # For comprehension questions 
-df = pd.read_csv("QUESTION_FOLDER_PATH" + "CSV_FILE_NAME" + ".csv", header=0, index_col=0)
+df = pd.read_csv("PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv", header=0, index_col=0)
 df['output'] = ''
 df['check'] = ''
 df['error'] = ''
@@ -41,4 +40,4 @@ for i in range(len(df)//2):
             )
   df.iloc[i*2, 8] = res["choices"][0]["message"]["content"]
   
-df.to_csv("RESULT_FOLDER_PATH" + "CSV_FILE_NAME" + ".csv")
+df.to_csv("PATH_TO_RESULT_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv")
