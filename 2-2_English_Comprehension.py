bc505e1036d91e95337269ddb5357d432538c8a4
Yudai TANAKA <63273927+yudaitanaka1026@users.noreply.github.com>
2023-12-13 01:25:35 +0900

    Add files via upload

--- 2-2_English_Comprehension.py
+++ 2-2_English_Comprehension.py
@@ -13,48 +13,57 @@ Setup requires:
 """
 
 # Install the packages
-!pip install openai
 import openai
 import pandas as pd
+
 openai.api_key = "YOUR_OPENAI_API_KEY"
 
 # Translation the original Japanese sentences into English ones and answer the translated questions
-# For comprehension questions 
-df = pd.read_csv("PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv", header=0, index_col=0)
-df['english'] = ''
-df['output'] = ''
-df['check'] = ''
-df['error'] = ''
-for i in range(len(df)//2):
-  english = []
-  english = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          {
-              "role": "system",
-           "content": "Translate the following Japanese into English."
-           },
-           {
-               "role": "user",
-            "content": "Q1:"+df.iloc[i*2, 3]+df.iloc[i*2, 4]+"Q2:"+df.iloc[i*2+1, 3]+df.iloc[i*2+1, 4]
-            }],
-            temperature=0
-            )
-  df.iloc[i*2, 7] = english["choices"][0]["message"]["content"]
-    
-  res = []
-  res = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          {
-              "role": "system",
-           "content": "Answer the following questions with reasons."
-           },
-           {"role": "user",
-            "content": df.iloc[i*2, 7]
-            }],
-            temperature=0
-            )
-  df.iloc[i*2, 8] = res["choices"][0]["message"]["content"]
-  
+# For comprehension questions
+df = pd.read_csv(
+    "PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv",
+    header=0,
+    index_col=0,
+)
+df["english"] = ""
+df["output"] = ""
+df["check"] = ""
+df["error"] = ""
+for i in range(len(df) // 2):
+    english = []
+    english = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "Translate the following Japanese into English.",
+            },
+            {
+                "role": "user",
+                "content": "Q1:"
+                + df.iloc[i * 2, 3]
+                + df.iloc[i * 2, 4]
+                + "Q2:"
+                + df.iloc[i * 2 + 1, 3]
+                + df.iloc[i * 2 + 1, 4],
+            },
+        ],
+        temperature=0,
+    )
+    df.iloc[i * 2, 7] = english["choices"][0]["message"]["content"]
+
+    res = []
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "Answer the following questions with reasons.",
+            },
+            {"role": "user", "content": df.iloc[i * 2, 7]},
+        ],
+        temperature=0,
+    )
+    df.iloc[i * 2, 8] = res["choices"][0]["message"]["content"]
+
 df.to_csv("PATH_TO_RESULT_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv")
