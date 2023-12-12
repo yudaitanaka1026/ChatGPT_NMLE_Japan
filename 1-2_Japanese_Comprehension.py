0f5a2351811eb9fc5eefccf79584b431d06059e0
Yudai TANAKA <63273927+yudaitanaka1026@users.noreply.github.com>
2023-12-13 01:25:03 +0900

    Add files via upload

--- 1-2_Japanese_Comprehension.py
+++ 1-2_Japanese_Comprehension.py
@@ -13,31 +13,39 @@ Setup requires:
 """
 
 # Install the packages
-!pip install openai
 import openai
 import pandas as pd
+
 openai.api_key = "YOUR_OPENAI_API_KEY"
 
 # Answer the Original Japanese questions
-# For comprehension questions 
-df = pd.read_csv("PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv", header=0, index_col=0)
-df['output'] = ''
-df['check'] = ''
-df['error'] = ''
-for i in range(len(df)//2):
-  res = []
-  res = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          {
-                "role": "system",
-             "content": "次の質問に理由を添えて答えよ"
-             },
-             {"role": "user",
-            "content": "Q1:"+df.iloc[i*2, 3]+df.iloc[i*2, 4]+"Q2:"+df.iloc[i*2+1, 3]+df.iloc[i*2+1, 4]
-            }],
-            temperature=0
-            )
-  df.iloc[i*2, 8] = res["choices"][0]["message"]["content"]
-  
+# For comprehension questions
+df = pd.read_csv(
+    "PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv",
+    header=0,
+    index_col=0,
+)
+df["output"] = ""
+df["check"] = ""
+df["error"] = ""
+for i in range(len(df) // 2):
+    res = []
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "次の質問に理由を添えて答えよ"},
+            {
+                "role": "user",
+                "content": "Q1:"
+                + df.iloc[i * 2, 3]
+                + df.iloc[i * 2, 4]
+                + "Q2:"
+                + df.iloc[i * 2 + 1, 3]
+                + df.iloc[i * 2 + 1, 4],
+            },
+        ],
+        temperature=0,
+    )
+    df.iloc[i * 2, 8] = res["choices"][0]["message"]["content"]
+
 df.to_csv("PATH_TO_RESULT_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv")
