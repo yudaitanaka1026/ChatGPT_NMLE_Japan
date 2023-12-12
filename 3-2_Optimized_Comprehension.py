6990c8849f2b9091ed1bd54f8abb316d395b7157
Yudai TANAKA <63273927+yudaitanaka1026@users.noreply.github.com>
2023-12-13 01:25:56 +0900

    Add files via upload

--- 3-2_Optimized_Comprehension.py
+++ 3-2_Optimized_Comprehension.py
@@ -14,55 +14,72 @@ Setup requires:
 """
 
 # Install the packages
-!pip install openai
 import openai
 import pandas as pd
+
 openai.api_key = "YOUR_OPENAI_API_KEY"
 
 # Load the optimized prompts
 df_p = pd.read_csv("PATH_TO_PROMPT_CSV_FILE", header=0, index_col=0)
 df_p.head()
 
-tra = [df_p.iloc[0, 0], df_p.iloc[1, 0], df_p.iloc[0, 0], df_p.iloc[0, 0], df_p.iloc[1, 0], df_p.iloc[1, 0]]
-ex = [df_p.iloc[0, 1], df_p.iloc[1, 1], df_p.iloc[0, 1], df_p.iloc[0, 1], df_p.iloc[1, 1], df_p.iloc[1, 1]]
+tra = [
+    df_p.iloc[0, 0],
+    df_p.iloc[1, 0],
+    df_p.iloc[0, 0],
+    df_p.iloc[0, 0],
+    df_p.iloc[1, 0],
+    df_p.iloc[1, 0],
+]
+ex = [
+    df_p.iloc[0, 1],
+    df_p.iloc[1, 1],
+    df_p.iloc[0, 1],
+    df_p.iloc[0, 1],
+    df_p.iloc[1, 1],
+    df_p.iloc[1, 1],
+]
 
 # Translation the original Japanese sentences into plain English and answer the translated questions
 # For comprehension questions
-df = pd.read_csv("PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv", header=0, index_col=0)
-df['english'] = ''
-df['output'] = ''
-df['check'] = ''
-df['error'] = ''
-for i in range(len(df)//2):
-  english = []
-  english = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          {
-              "role": "system",
-           "content": df_p.iloc[2, 0]
-           },
-           {
-               "role": "user",
-            "content": "Q1:"+df.iloc[i*2, 3]+df.iloc[i*2, 4]+"Q2:"+df.iloc[i*2+1, 3]+df.iloc[i*2+1, 4]
-            }],
-            temperature=0
-            )
-  df.iloc[i*2, 7] = english["choices"][0]["message"]["content"]
-  
-  res = []
-  res = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-          {"role": "system",
-           "content": df_p.iloc[2, 1]
-           },
-           {
-               "role": "user",
-            "content": df.iloc[i*2, 7]
-            }],
-            temperature=0
-            )
-  df.iloc[i*2, 8] = res["choices"][0]["message"]["content"]
-  
+df = pd.read_csv(
+    "PATH_TO_QUESTION_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv",
+    header=0,
+    index_col=0,
+)
+df["english"] = ""
+df["output"] = ""
+df["check"] = ""
+df["error"] = ""
+for i in range(len(df) // 2):
+    english = []
+    english = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": df_p.iloc[2, 0]},
+            {
+                "role": "user",
+                "content": "Q1:"
+                + df.iloc[i * 2, 3]
+                + df.iloc[i * 2, 4]
+                + "Q2:"
+                + df.iloc[i * 2 + 1, 3]
+                + df.iloc[i * 2 + 1, 4],
+            },
+        ],
+        temperature=0,
+    )
+    df.iloc[i * 2, 7] = english["choices"][0]["message"]["content"]
+
+    res = []
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": df_p.iloc[2, 1]},
+            {"role": "user", "content": df.iloc[i * 2, 7]},
+        ],
+        temperature=0,
+    )
+    df.iloc[i * 2, 8] = res["choices"][0]["message"]["content"]
+
 df.to_csv("PATH_TO_RESULT_FOLDER" + "YOUR_QUESTION_CSV_FILE_NAME" + ".csv")
